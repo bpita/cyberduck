f7868ebb9488007e7fdc2cd2d5786939b4860458
bpita <b.pita.f@gmail.com>
2019-02-27 21:13:18 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var f = function ($) {
+	$(document).ready(function(){
+		$('ul li a').click(function(){
+			$('li a').removeClass("active");
+			$(this).addClass("active");
+		});
+	});
+}(jQuery);
